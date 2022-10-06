83cdd30ac7ee7cdae99da32f7267b36771acbc20
Tincho Develops <97058453+tinchopapa56@users.noreply.github.com>
2022-10-06 17:01:08 -0300

    textos

--- src/components/views/Navbar/Navbar.jsx
+++ src/components/views/Navbar/Navbar.jsx
@@ -6,9 +6,9 @@ export default function Navbar() {
   return (
     <div className='navbar'>
         <img id="logo" src={Logo} />
-        <h2>create fleet</h2>
-        <h2>statke+claim</h2>
-        <h2>eteranl</h2>
+        <h2>fleet</h2>
+        <h2>stake</h2>
+        <h2>Eteranl</h2>
     </div>
   )
 }
