9d10fa431e46d9ce9c153f017c82b51a1f386a26
Tincho Develops <97058453+tinchopapa56@users.noreply.github.com>
2022-10-06 17:01:51 -0300

    better sections

--- src/components/views/Navbar/Navbar.jsx
+++ src/components/views/Navbar/Navbar.jsx
@@ -7,8 +7,12 @@ export default function Navbar() {
     <div className='navbar'>
         <img id="logo" src={Logo} />
         <h2>fleet</h2>
-        <h2>stake</h2>
-        <h2>Eteranl</h2>
+        
+        <div>
+          <h2>stake</h2>
+          <h2>Eteranl</h2>
+        </div>
+        <img />
     </div>
   )
 }
